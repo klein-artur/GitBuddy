d54943c0bb3259a5ae77fc9b076d584b04300cec
Artur Hellmann <artur.hellmann@luca-app.de>
2022-12-29 15:12:00 +0100

    Initial Commit

---
+++ GitBuddy/GitBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitBuddyApp.swift
+//  GitBuddy
+//
+//  Created by Artur Hellmann on 29.12.22.
+//
+
+import SwiftUI
+
+@main
+struct GitBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
