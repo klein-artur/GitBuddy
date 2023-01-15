ca0ed1898d7483445a95ad63129d5c5d412c2e8d
Artur Hellmann <artur.hellmann@yahoo.de>
2023-01-15 22:48:00 +0100

    Error alert is not alert, it's a sheet

--- GitBuddy/Views/Helper/ErrorAlert.swift
+++ GitBuddy/Views/Helper/ErrorAlert.swift
@@ -12,29 +12,51 @@ struct GitErrorAlert: ViewModifier {
     
     func body(content: Content) -> some View {
         content
-            .alert(
-                "",
-                isPresented: Binding(get: {
-                    !gitError.isEmpty
+            .sheet(
+                item: Binding(get: {
+                    gitError.isEmpty ? nil : gitError
                 }, set: { value in
-                    if !value {
-                        gitError = ""
-                    }
-                }),
-                actions: {
-                    Button {
-                        AppDelegate.shared?.reload()
-                    } label: {
-                        Text("OK")
-                    }
-                }, message: {
+                    gitError = value ?? ""
+                })) { error in
+                VStack(alignment: .trailing) {
                     Text(gitError)
-                })
+                        .lineLimit(nil)
+                        .frame(width: 500, height: 400, alignment: .topLeading)
+                    Button("Ok") {
+                        gitError = ""
+                    }.keyboardShortcut(.defaultAction)
+                }
+                .padding()
+            }
+//            .alert(
+//                "",
+//                isPresented: Binding(get: {
+//                    !gitError.isEmpty
+//                }, set: { value in
+//                    if !value {
+//                        gitError = ""
+//                    }
+//                }),
+//                actions: {
+//                    Button {
+//                        AppDelegate.shared?.reload()
+//                    } label: {
+//                        Text("OK")
+//                    }
+//                }, message: {
+//                    Text(gitError)
+//                })
     }
 }
 
 extension View {
     func gitErrorAlert(gitError: Binding<String>) -> some View {
         modifier(GitErrorAlert(gitError: gitError))
+    }
+}
+
+extension String: Identifiable {
+    public var id: String {
+        self
     }
 }
